94572f6df3650b951e9811f30454014ca20ccd87
Jeyapriyan Nadaraja <jeyapiriyan1992@gmail.com>
2022-05-04 00:11:47 +0530

    feat: fix session issues

--- README.md
+++ README.md
@@ -2,15 +2,29 @@
 
 ## Installation
 
+
+
 ### Install the packages
 
 ```bash
 $ npm install
 ```
 
+### Configuration
+
+<br>
+set the following environment variables
+
+export CLIENT_ID= < GCP API application client ID>  
+export CLIENT_SECRET=< GCP API application client secret>  
+export LOGIN_REDIRECT_URL=< GCP API application post login redirect url >  eg: "/auth/google/callback"  
+export SECRET_KEY=< Session secret key>
+export PORT=< Node JS server port>
+
+
 ### Run the server
 
 ```bash
-$ npm run start
+$ node index.js
 
 ```
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const homeRouter = require('./routes/home')
 const authRouter = require('./routes/auth')
-const passportConfig = require('./middleware/passport')
+const __ = require('./middleware/passport')
 const passport = require('passport')
-const cookieSession = require('cookie-session')
+const session = require('express-session')
 const nunjucks = require('nunjucks')
 const fileUpload = require('express-fileupload')
 
 // init app
 let app = express()
-const port =  process.env.PORT
-app.listen(process.env.PORT, '0.0.0.0',  ()=> console.log(`server is running on ${process.env.PORT}`));
+const port = process.env.PORT
+app.listen(process.env.PORT, '0.0.0.0', () => console.log(`server is running on ${process.env.PORT}`));
 
 
 // init view
@@ -22,15 +22,25 @@ nunjucks.configure('views', {
 // init static
 app.use('/static', express.static('public'))
 
-
 // init session
-app.use(cookieSession({
-    keys: [process.env.SECRET_KEY]
-}))
+app.use(session({
+    secret: process.env.SECRET_KEY,       //decode or encode session
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 2 * 60 * 1000
+    }
+}));
 
 // init passport
 app.use(passport.initialize())
 app.use(passport.session())
+
+//current User
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+})
 
 // file upload
 app.use(fileUpload());
